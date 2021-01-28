7634471f83c058315152e81b078bc1ab6e4859be
melon9586 <melon9586@gmail.com>
2021-01-28 23:53:00 +0700

    EX4
    Grading1

---
+++ Excercise4_Nattanon_N.py
@@ -0,0 +1,10 @@
+FE = 87
+GB = 56
+ICS = 77
+CP = 68
+print ("           --- Your Score ---")
+print ("Foundation English               :",FE)
+print ("General Business                 :",GB)
+print ("Introduction to Computer Systems :",ICS)
+print ("Computer Programming             :",CP)
+
